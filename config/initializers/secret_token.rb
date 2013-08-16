632d263c7af0b7530d450f0373552a9dd5544655
natsteinmetz <nathalie.steinmetz@gmail.com>
2013-08-16 12:36:29 -0700

    setup project, setup vagrant box

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwsBlog::Application.config.secret_token = 'd4828de9e22e604725db199a9f4e639c6f03c3dfe64a1ad67433dbc5a4d4c130faa6c086c2d18eb01fccb5cdd5d6e7abb48892672c8c022f5aa47fa3c592494d'
